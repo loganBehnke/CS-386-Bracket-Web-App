453d624644fec3329006055ec7794cee1e7b9103
bdb253 <bdb253@nau.edu>
2019-03-14 17:11:57 -0700

    added bracket generation and logans css

--- src/BestBracketz/urls.py
+++ src/BestBracketz/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth.views import LoginView, LogoutView
 
-from Bracket.views import BracketListView, BracketDetailView
+from Bracket.views import BracketListView, BracketDetailView, BracketCreateView, join_bracketz, bracket_gen
 from player.views import PlayerListView, PlayerDetailView, RegisterView, PlayerCreateView
 from RegisteredTeams.views import TeamDetailView, TeamListView, TeamCreateView, join_team
 
@@ -38,5 +38,9 @@ urlpatterns = [
     url(r'^players/sign-up/$', PlayerCreateView.as_view(), name='player-sign-up'),
     url(r'^players/(?P<slug>[\w-]+)/$', PlayerDetailView.as_view()),
     url(r'^bracketz/$', BracketListView.as_view(), name='bracketz'),
+    url(r'^bracketz/creation/$', BracketCreateView.as_view(), name='bracketz-creation'),
     url(r'^bracketz/(?P<slug>[\w-]+)/$', BracketDetailView.as_view()),
+    url(r'^bracketz/generate/(?P<slug>[\w-]+)/$', bracket_gen)
+    #url(r'^bracketz/join-bracketz/(?P<slug>[\w-]+)/$', join_bracketz), #TODO in future version
+
 ]
