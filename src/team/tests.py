5f8ad9f46acef4571b9c79e8d22a734ac13c55ab
Dylan <Djs554@nau.edu>
2019-04-19 01:37:23 -0700

    Adding tests for teams. Team creation test added

--- src/team/tests.py
+++ src/team/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from team.models import Team
+from django.core.urlresolvers import reverse
+from django.contrib.auth.admin import User
+from player.forms import TeamCreateForm, JoinTeamForm
+from player.views import TeamListView
 
 # Create your tests here.
+class TeamTest(TestCase):
+    #Team creation
+    def create_team(self):
+        return Team.objects.create_team(name = 'testTeam')
+  
+    def test_team_creation(self):
+        t = self.create_team()
+        self.assertTrue(isinstance(t, Team))
+        self.assertEqual(t.name, 'testTeam')
+        
