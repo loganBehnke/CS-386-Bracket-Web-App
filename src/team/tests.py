9d98c9db4ee7f9e7db07d38bd64c29ffaaec7629
Dschreiner98 <djs554@nau.edu>
2019-04-19 17:01:00 -0700

    Dylan (#14)
    Looks good KW
    
    * removed mac files
    
    * Added footer info
    
    * Many css changes. Corrections to static folder can be seen here as well
    
    * Revert "Added footer info"
    
    This reverts commit 58881d26e10f3de6024f36b12821c2cb16ee8257.
    
    * Revert "Revert "Added footer info""
    
    This reverts commit 2d3e57f325539b766880037d4e04dd88f76501f2.
    
    * Revert "Many css changes. Corrections to static folder can be seen here as well"
    
    This reverts commit fe9bf8072de8cc11b87bec32920f66e6c36bf705.
    
    * Fixed css comit issue
    
    * Small css fixes for footer
    
    * Adding tests for teams. Team creation test added
    
    * Fixed Import typo
    
    * Form, View, and Team tests added and checked
    
    * Finished all tests for the team section

--- src/team/tests.py
+++ src/team/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from player.models import Player
+from team.models import Team
+from django.core.urlresolvers import reverse
+from django.contrib.auth.admin import User
+from team.forms import TeamCreateForm, JoinTeamForm
+from team.views import TeamListView
 
 # Create your tests here.
+class TeamTest(TestCase):
+    #Team creation
+    def create_team(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        player = Player.objects.create(account=user, gameName = 'testPlayer')
+        return Team.objects.create(name = 'testTeam', teamCaptin = player)
+  
+    def test_team_creation(self):
+        t = self.create_team()
+        self.assertTrue(isinstance(t, Team))
+        self.assertEqual(t.name , 'testTeam')
+        self.assertEqual(t.title , 'testTeam')
+
+    #View test
+    def test_team_list_view(self):
+        t = self.create_team()
+        url = reverse("teams")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(bytes(t.title, 'utf-8'), response.content)
+
+    def test_team_detail_view(self):
+        t = self.create_team()
+        url = '/teams/' + t.slug + '/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(bytes(t.title, 'utf-8'), response.content)
+
+    #TODO Join team view
+
+    #Form test
+    def test_create_team_form(self):
+        t = self.create_team()
+        data = {'name': t.name}
+        form = TeamCreateForm(data = data)
+        self.assertTrue(form.is_valid())
+
+    def test_join_team_form(self):
+            user = User.objects.create_user(username='testuser3', password='12345')
+            player = Player.objects.create(account=user, gameName = 'testPlayer')
+            t = self.create_team()
+            data = {'players': player}
+            form = JoinTeamForm(data)
+            self.assertTrue(form.is_valid())
